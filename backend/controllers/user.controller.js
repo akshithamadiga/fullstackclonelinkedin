8d0fb65b96bd2bb4a386c90ebfb768409e10a205
akshithamadiga <akshithamadiga@gmail.com>
2026-08-02 13:23:26 +0530

    Complete backend implementation for LinkedIn clone

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,10 +1,41 @@
 import Profile from "../models/profile.model.js";
 import User from "../models/user.model.js";
+import ConnectionRequest from "../models/connections.model.js";
 import crypto from 'crypto';
 
 import bcrypt from 'bcrypt';
+import PDFDocument from 'pdfkit';
+import fs from "fs";
 
 
+
+const convertUserDataToPDF =async (userData) => {
+    const doc = new PDFDocument();
+
+    const outputPath = crypto.randomBytes(32).toString("hex") + ".pdf";
+    const stream = fs.createWriteStream(`uploads/${outputPath}`);
+    doc.pipe(stream);
+
+    doc.image(`uploads/${userData.userId.profilePicture}`, { align: "center",width: 100 });
+    doc.fontSize(14).text(`Name: ${userData.userId.name}`);
+    doc.fontSize(14).text(`Username: ${userData.userId.username}`);
+    doc.fontSize(14).text(`Email: ${userData.userId.email}`);
+    doc.fontSize(14).text(`Bio: ${userData.bio}`);
+    doc.fontSize(14).text(`Current Position: ${userData.currentPosition}`);
+
+    doc.fontSize(14).text("Past Work:");
+
+    userData.pastWork.forEach((work, index) => {
+        doc.fontSize(14).text(`Company Name: ${work.companyName}`);
+        doc.fontSize(14).text(`Position: ${work.position}`);
+        doc.fontSize(14).text(`Years: ${work.years}`);
+    })
+    
+    doc.end();
+
+    return outputPath;
+
+}
 
 export const register = async (req, res) => {
 
@@ -159,9 +190,6 @@ export const register = async (req, res) => {
 };
 
 
-
-
-
 export const updateProfileData = async (req, res) => {
 
     try {
@@ -184,5 +212,211 @@ export const updateProfileData = async (req, res) => {
 
     } catch (error) {
         return res.status(500).json({ message: error.message });
+    }
+};
+
+export const getAllUserProfile = async (req, res) => {
+    try {
+
+        const profiles = await Profile.find().populate("userId","name username email profilePicture");
+
+        return res.json({ profiles });
+
+    } catch (error) {
+
+        return res.status(500).json({message: error.message});
+
+    }
+};
+
+
+export const downloadProfile = async (req, res) => {
+    const user_id = req.query.id;
+
+    const userProfile = await Profile.findOne({ userId: user_id })
+    .populate("userId", "name username email profilePicture" );
+
+    let outputPath = await convertUserDataToPDF(userProfile);
+
+    return res.json({ message: outputPath });
+};
+
+
+
+
+
+
+
+
+
+
+
+
+export const sendConnectionRequest = async (req, res) => {
+
+    const { token, connectionId } = req.body;
+
+    try {
+
+        const user = await User.findOne({ token });
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+
+        }
+
+        const connectionUser = await User.findOne({ _id: connectionId });
+
+        if (!connectionUser) {
+            return res.status(404).json({ message: "Connection User not found" });
+        }
+
+        const existingRequest = await ConnectionRequest.findOne(
+            {
+            userId: user._id,
+            connectionId: connectionUser._id
+        }
+    )
+
+
+        if (existingRequest) {
+            return res.status(400).json({ message: "Request already sent" });
+        }
+
+        const request = new ConnectionRequest({
+            userId: user._id,
+            connectionId: connectionUser._id
+        });
+
+        await request.save();
+
+        return res.json({ message: "Request Sent" });
+
+
+
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
+}
+
+
+export const getMyConnectionsRequests = async (req, res) => {
+    const { token } = req.body;
+
+    try {
+        const user = await User.findOne({ token });
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        const connections = await ConnectionRequest.find({ userId: user._id })
+        .populate("connectionId","name username email profilePicture");
+
+        return res.json({ connections })
+
+
+
+
+
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
+}
+
+
+
+export const whatAreMyConnections = async (req, res) => {
+
+    const { token } = req.body;
+
+    try {
+
+        const user = await User.findOne({ token });
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" })
+        }
+
+        const connections = await ConnectionRequest.find({ connectionId: user._id })
+        .populate("userId","name username email profilePicture");
+
+        return res.json({ connections });
+
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
+
+}
+
+
+
+export const acceptConnectionRequest = async (req, res) => {
+
+    const { token, requestId, action_type } = req.body;
+
+    try {
+
+        const user = await User.findOne({ token });
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        const connection = await ConnectionRequest.findOne({ _id: requestId });
+
+        if (!connection) {
+            return res.status(404).json({ message: "Connection not found" });
+        }
+
+        if (action_type === "accept") {
+            connection.status_accepted = true;
+        } else {
+            connection.status_accepted = false;
+        }
+
+        await connection.save();
+        return res.json({ message: "Request Updated" });
+
+    } catch (err) {
+        return res.status(500).json({ message: err.message })
+    }
+}
+
+
+
+export const commentPost = async (req, res) => {
+
+    const { token, post_id, commentBody } = req.body;
+
+    try {
+
+        const user = await User.findOne({ token: token }).select("_id");
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        const post = await Post.findOne({
+            _id: post_id
+        });
+
+        if (!post) {
+            return res.status(404).json({ message: "Post not found" });
+        }
+
+        const comment = new Comment({
+            userId: user._id,
+            postId: post_id,
+            comment: commentBody
+        });
+
+       
+        await comment.save();
+
+        return res.json({ message: "Comment Added" });
+
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
     }
 };
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,5 +1,8 @@
 import { Router } from "express";
-import { login, register, uploadProfilePicture, updateUserProfile, getUserAndProfile, updateProfileData } from "../controllers/user.controller.js";
+import { login, register, uploadProfilePicture, updateUserProfile, getUserAndProfile, updateProfileData, getAllUserProfile ,downloadProfile,
+    sendConnectionRequest, getMyConnectionsRequests, whatAreMyConnections, acceptConnectionRequest} from "../controllers/user.controller.js";
+
+
 import multer from "multer";
 
 const router = Router();
@@ -24,6 +27,12 @@ router.route("/register").post(register);
 router.route("/login").post(login);
 router.route("/user_update").post(updateUserProfile);
 router.route("/get_user_and_profile").get(getUserAndProfile)
-router.route("/update_profile_data").post(updateProfileData)
+router.route("/update_profile_data").post(updateProfileData);
+router.route("/user/get_all_users").get(getAllUserProfile);
+router.route("/user/download_resume").get(downloadProfile);
+router.route("/user/send_connection_request").post(sendConnectionRequest);
+router.route("/user/getConnectionRequests").get(getMyConnectionsRequests);
+router.route("/user/user_connection_request").get(whatAreMyConnections);
+router.route("/user/accept_connection_request").post(acceptConnectionRequest);
 
 export default router;
